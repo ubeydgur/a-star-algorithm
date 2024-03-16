3257677d3f85c2873f0adf516f9b9aa7a33a7cfb
Ubeyd Gür <ubeydgur41@gmail.com>
2024-03-16 22:00:44 +0300

    Add continuous calling of the function and edited the screen

--- src/main/java/com/example/astaralgorithm/HelloApplication.java
+++ src/main/java/com/example/astaralgorithm/HelloApplication.java
@@ -1,22 +1,36 @@
 package com.example.astaralgorithm;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    Screen mainScreen = new Screen();
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), e -> {
+            tick();
+        }));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+
+        stage.setTitle("AStar");
+        stage.setScene(mainScreen.getScene());
         stage.show();
     }
-
+    public void tick() {
+        mainScreen.search();
+    }
     public static void main(String[] args) {
         launch();
     }
